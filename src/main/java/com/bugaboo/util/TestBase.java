b7522e8c5fca5f4138fe69bc7d34dd5309f02026
Vladimir902 <vladimir.malakovski@kinandcarta.com>
2024-10-03 18:41:37 +0200

    Updated .gitignore for abstraction of .properties configuration files and remove unused import of TestBase.java file

--- src/main/java/com/bugaboo/util/TestBase.java
+++ src/main/java/com/bugaboo/util/TestBase.java
@@ -11,8 +11,6 @@ import org.slf4j.LoggerFactory;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Parameters;
-
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.io.FileHandler;
 
