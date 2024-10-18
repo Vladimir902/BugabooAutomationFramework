6f7c724fb32d9226ec2ab6d2ba2b49e0c774a82b
Vladimir902 <vladimir.malakovski@kinandcarta.com>
2024-10-18 16:23:34 +0200

    removed unused import packages

--- src/main/java/com/bugaboo/pages/HomePage.java
+++ src/main/java/com/bugaboo/pages/HomePage.java
@@ -3,10 +3,6 @@ package com.bugaboo.pages;
 import org.openqa.selenium.*;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.time.Duration;
 
 public class HomePage {
 
